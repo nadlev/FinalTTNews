5f785888153e52062b040e8ab162c51165721d8b
nadlev <nadiya.levytska.jr@gmail.com>
2023-02-16 11:53:25 +0200

    Initial Commit

---
+++ FinalTTNews/FinalTTNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalTTNewsApp.swift
+//  FinalTTNews
+//
+//  Created by Надежда Левицкая on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalTTNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
